a1c2627d921ec0f0aa6d7ba2abf2909082590ad3
FatCatX <garretsimpson@yahoo.com>
2021-07-08 03:27:24 -0700

    Bump version to 0.2.0

--- main.js
+++ main.js
@@ -9,16 +9,17 @@
 import { Shape } from "./shape.js";
 import { Spu } from "./spu.js";
 
-const APP_NAME = "Shape Processing Unit 0.1";
+const APP_NAME = "Shape Processing Unit 0.2.0";
 
 function main() {
   console.log(APP_NAME);
   console.log(Date());
+  console.log("");
 
   // Shape.runTests();
   // Shape.testPerf();
   // Shape.testLogo();
-  Spu.runTests();
+  // Spu.runTests();
   Spu.runSearch();
 }
 

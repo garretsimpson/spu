d10c4a1be18d6316d8194243e1cc72eb14f44bf1
FatCatX <garretsimpson@yahoo.com>
2022-05-17 09:30:45 -0700

    Update MyTmam
    - Refactored makeLogos().
    - Added deconstructor for "original" TMAM.

--- mytmam.js
+++ mytmam.js
@@ -11,12 +11,55 @@ import { Shape } from "./shape.js";
 import { Fileops } from "./fileops.js";
 
 export class MyTmam {
+  static init() {
+    MyTmam.makeLogos();
+  }
+
+  /**
+   * Make logo and mask constants.
+   * There are four positions (ENWS), three sizes (2..4), and two types of masks.
+   */
+  static makeLogos() {
+    const LOGO = [[], [], [0x21, 0x12], [0x121, 0x212], [0x2121, 0x1212]];
+    const MASK_X = [[], [], [0x33, 0x33], [0x333, 0x333], [0x3333, 0x3333]];
+    const MASK_Y = [[], [], [0x33, 0x33], [0x133, 0x233], [0x2333, 0x1333]];
+
+    MyTmam.LOGOS_X = MyTmam.makeLogoCodes(LOGO, MASK_X);
+    MyTmam.LOGOS_Y = MyTmam.makeLogoCodes(LOGO, MASK_Y);
+  }
+
+  /**
+   * @param {number} shape
+   * @param {number} part
+   * @returns {number[][][]}
+   */
+  static makeLogoCodes(logos, masks) {
+    const codes = [];
+    let sizes, values;
+    for (let pos = 0; pos < 4; ++pos) {
+      sizes = [];
+      for (let size = 2; size <= 4; ++size) {
+        values = [];
+        for (let num = 0; num < 2; ++num) {
+          values.push({
+            logo: Shape.rotateCode(logos[size][num], pos),
+            mask: Shape.rotateCode(masks[size][num], pos),
+          });
+        }
+        sizes[size] = values;
+      }
+      codes[pos] = sizes;
+    }
+    return codes;
+  }
+
   static test() {
     const knownShapes = new Map();
     const unknownShapes = new Map();
 
-    const possibleShapes = [];
     Shape.init();
+    MyTmam.init();
+    const possibleShapes = [];
     for (let code = 0; code <= 0xffff; ++code) {
       if (Shape.isPossible(code)) possibleShapes.push(code);
     }
@@ -26,12 +69,13 @@ export class MyTmam {
       (code) => Shape.keyCode(code) == code
     );
     const complexShapes = keyShapes.filter((code) => !Shape.canStackAll(code));
-    complexShapes.forEach((code) => unknownShapes.set(code, { code }));
+    // complexShapes.forEach((code) => unknownShapes.set(code, { code }));
 
-    // const testShapes = [0x1, 0x21, 0x31, 0xa5];  // basic test shapes
+    // const testShapes = [0x1, 0x21, 0x31, 0xa5]; // basic test shapes
     // const testShapes = [0x0361, 0x1361, 0x1634, 0x17a4, 0x1b61, 0x36c2, 0x37a4]; // must use seat joint
     // const testShapes = [0x1634, 0x3422]; // 3-logo and fifth layer
-    // testShapes.forEach((code) => unknownShapes.set(code, { code }));
+    const testShapes = [0x0178, 0x0361, 0x1622]; // hat and seat
+    testShapes.forEach((code) => unknownShapes.set(code, { code }));
 
     console.log("Knowns:", knownShapes.size);
     console.log("Unknowns:", unknownShapes.size);
@@ -39,7 +83,7 @@ export class MyTmam {
 
     let result;
     for (let code of Array.from(unknownShapes.keys())) {
-      result = MyTmam.deconstruct(code);
+      result = MyTmam.deconstruct0(code);
       if (!result) {
         console.log("NOT FOUND", Shape.pp(code));
       } else {
@@ -157,6 +201,24 @@ export class MyTmam {
   /**
    * @param {number} shape
    * @param {number} part
+   * @returns {boolean}
+   */
+  static hasHat(shape, part) {
+    // find top of part
+    let top = 0;
+    let mask = 0xf000;
+    while (top == 0 && mask > 0) {
+      top = part & mask;
+      mask >>>= 4;
+    }
+    // look for top on top of top
+    top <<= 4;
+    return (shape & top) != 0;
+  }
+
+  /**
+   * @param {number} shape
+   * @param {number} part
    * @returns {number}
    */
   static removePart(shape, part) {
@@ -165,44 +227,40 @@ export class MyTmam {
 
   /**
    * Find half-logo parts
-   * - There are four positions (ENWS), three sizes (2..4), and two directions (forward, backwards).
-   * - First find the largest part in each position, and then return the first one found in the given direction.
    * @param {number} shape
    * @param {object} config
    * @returns {Array<number>}
    */
-  static findLogo(shape, config) {
-    const LOGO = [[], [], [0x21, 0x12], [0x121, 0x212], [0x2121, 0x1212]];
-    const MASK_X = [[], [], [0x33, 0x33], [0x333, 0x333], [0x3333, 0x3333]];
-    const MASK_Y = [[], [], [0x33, 0x33], [0x133, 0x233], [0x2333, 0x1333]];
-    const MASK = config.seat ? MASK_Y : MASK_X;
-    const MAX = config.seat ? 3 : 4;
-    // TODO: Smarter 5th layer so that MAX can be always 4.
-
-    // TODO: Refactor this to a constant, initialized once
-    const codes = []; // codes[pos][size][num]
-    let sizes, values;
+  static findLogos(shape, config) {
+    const LOGOS = MyTmam.LOGOS_X;
+    let result = [];
     for (let pos = 0; pos < 4; ++pos) {
-      sizes = [];
-      for (let size = 2; size <= MAX; ++size) {
-        values = [];
-        for (let num = 0; num < 2; ++num) {
-          values.push({
-            logo: Shape.rotateCode(LOGO[size][num], pos),
-            mask: Shape.rotateCode(MASK[size][num], pos),
-          });
-        }
-        sizes[size] = values;
+      for (const { logo, mask } of LOGOS[pos][config.size]) {
+        if ((shape & mask) == logo) result.push(logo);
       }
-      codes[pos] = sizes;
     }
+    if (config.hat === true)
+      result = result.filter((part) => MyTmam.hasHat(shape, part));
+    return result;
+  }
 
+  /**
+   * Find half-logo parts
+   * Finds the largest part in each position.
+   * @param {number} shape
+   * @param {object} config
+   * @returns {Array<number>}
+   */
+  static findBigLogos(shape, config) {
+    const LOGOS = config.seat ? MyTmam.LOGOS_Y : MyTmam.LOGOS_X;
+    // TODO: Smarter 5th layer so that MAX can be always 4.
+    const MAX = config.seat ? 3 : 4;
     const result = [];
     const found = [];
     for (let pos = 0; pos < 4; ++pos) {
       found.length = 0;
       for (let size = MAX; size >= 2; --size) {
-        for (const { logo, mask } of codes[pos][size]) {
+        for (const { logo, mask } of LOGOS[pos][size]) {
           if ((shape & mask) == logo) found.push(logo);
         }
         if (found.length > 0) {
@@ -216,6 +274,71 @@ export class MyTmam {
   }
 
   /**
+   * Deconstructor - "original" TMAM method
+   * @param {number} shape
+   * @returns {object}
+   */
+  static deconstruct0(targetShape) {
+    console.log("Deconstruct");
+    console.log(Shape.toShape(targetShape));
+    console.log(Shape.pp(targetShape));
+    console.log(Shape.graph(targetShape));
+
+    const configs = [
+      { size: 2, hat: true },
+      { size: 3, hat: true },
+      { size: 4 },
+      { size: 3 },
+      { size: 2 },
+    ];
+
+    let shape, part, logos;
+    const partList = [];
+    shape = MyTmam.add5th(targetShape);
+    while (!MyTmam.isEmpty(shape)) {
+      part = null;
+      if (MyTmam.noBottom(shape)) {
+        shape = MyTmam.dropBottom(shape);
+        continue;
+      }
+      if (MyTmam.isOneLayer(shape) || MyTmam.canStackBottom(shape)) {
+        part = MyTmam.getBottom(shape);
+        console.log("LAYER", Shape.pp(shape), Shape.pp([part]));
+      } else {
+        for (const config of configs) {
+          logos = MyTmam.findLogos(shape, config);
+          console.log(
+            ">LOGO",
+            Shape.pp(shape),
+            Shape.pp(logos),
+            `(${config.size}${config.hat == true ? ",hat" : ""})`
+          );
+          if (logos.length > 0) {
+            part = logos[0];
+            console.log("LOGO ", Shape.pp(shape), Shape.pp([part]));
+            break;
+          }
+        }
+      }
+      if (part == null) {
+        part = MyTmam.getBottom(shape);
+        console.log("EXTRA", Shape.pp(shape), Shape.pp([part]));
+      }
+      partList.push(part);
+      shape = MyTmam.removePart(shape, part);
+    }
+
+    const result = { code: targetShape, build: partList };
+    const found = MyTmam.tryBuild(result);
+    if (found) {
+      return result;
+    } else {
+      return null;
+    }
+  }
+
+  /**
+   * Deconstructor - kipy method
    * @param {number} shape
    * @returns {object}
    */
@@ -248,7 +371,7 @@ export class MyTmam {
         if (MyTmam.isOneLayer(shape) || MyTmam.canStackBottom(shape)) {
           part = MyTmam.getBottom(shape);
           console.log("LAYER", Shape.pp(shape), Shape.pp([part]));
-        } else if ((logos = MyTmam.findLogo(shape, config)).length > 0) {
+        } else if ((logos = MyTmam.findBigLogos(shape, config)).length > 0) {
           console.log(">LOGO", Shape.pp(logos));
           if (config.reverse) {
             part = logos[logos.length - 1];

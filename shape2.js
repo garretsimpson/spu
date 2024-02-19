37e19d854712be055a05901ec6aaceaababa6347
FatcatX <garretsimpson@yahoo.com>
2024-02-18 20:18:38 -0800

    Update shape2.js
    Added isOmni check

--- shape2.js
+++ shape2.js
@@ -8,13 +8,14 @@ export class Shape2 {
   static RECT = "R";
   static STAR = "S";
   static WIND = "W";
+  static PINS = "P";
+  static CRYS = "X";
 
   static SHAPE_TYPES = [Shape2.VOID, Shape2.CIRC, Shape2.RECT, Shape2.STAR, Shape2.WIND];
+  //   static SHAPE_TYPES = [Shape2.VOID, Shape2.CIRC, Shape2.RECT, Shape2.STAR, Shape2.WIND, Shape2.PINS, Shape2.CRYS];
   static NUM_TYPES = Shape2.SHAPE_TYPES.length;
   static NUM_SPOTS = 4;
   static MAX_VALUE = Shape2.NUM_TYPES ** Shape2.NUM_SPOTS - 1;
-  static LSV = Shape2.NUM_TYPES;
-  static MSV = Shape2.NUM_TYPES ** (Shape2.NUM_SPOTS - 1);
 
   static toNumber(code) {
     return code.toString(Shape2.NUM_TYPES).padStart(Shape2.NUM_SPOTS, "0");
@@ -40,11 +41,14 @@ export class Shape2 {
     );
   }
 
+  static LSV = Shape2.NUM_TYPES;
+  static MSV = Shape2.NUM_TYPES ** (Shape2.NUM_SPOTS - 1);
+
   /**
-   * Shift left and rotate one digit
+   * Left shift and rotate one digit
    * TODO: Can do this with Numbers or Strings.  I think numbers will be faster.
    * @param {Number} code
-   * @returns
+   * @returns {Number}
    */
   static rotateLeft(code) {
     const lsd = code % Shape2.MSV;
@@ -53,9 +57,9 @@ export class Shape2 {
   }
 
   /**
-   * Shift right and rotate one digit
+   * Right shift and rotate one digit
    * @param {Number} code
-   * @returns
+   * @returns {Number}
    */
   static rotateRight(code) {
     const lsd = code % Shape2.LSV;
@@ -67,7 +71,7 @@ export class Shape2 {
    * Return the smallest value of all possible rotations
    * TODO: Use an array of values
    * @param {Number} code
-   * @returns
+   * @returns {Number}
    */
   static keyCode(code) {
     let result = code;
@@ -79,12 +83,29 @@ export class Shape2 {
   }
 
   /**
+   * Returns true only if the shape has at most one of each type of quarter.
+   * @param {Number} code
+   * @returns {Boolean}
+   */
+  static isOmni(code) {
+    const number = Shape2.toNumber(code);
+    let num;
+    for (let value = 1; value <= Shape2.NUM_TYPES; ++value) {
+      // count the number of these values
+      num = Array.from(number).filter((v) => v == value).length;
+      if (num > 1) return false;
+    }
+    return true;
+  }
+
+  /**
    * Return all keys
    */
   static findKeys() {
     const keys = [];
     for (let code = 1; code <= Shape2.MAX_VALUE; ++code) {
-      if (code == Shape2.keyCode(code)) {
+      if (code == Shape2.keyCode(code) && Shape2.isOmni(code)) {
+        // if (code == Shape2.keyCode(code)) {
         keys.push(code);
       }
     }
@@ -107,11 +128,13 @@ export class Shape2 {
     const TESTS = [
       ["toString", [0], "----"],
       ["toString", [Shape2.MAX_VALUE], "WWWW"],
-      ["fromString", ["CCCC"], 156],
+      ["fromString", ["CCCC"], 125 + 25 + 5 + 1],
       ["rotateLeft", [Shape2.fromString("CRSW")], Shape2.fromString("RSWC")],
       ["rotateRight", [Shape2.fromString("CRSW")], Shape2.fromString("WCRS")],
       ["keyCode", [Shape2.fromString("CRSW")], Shape2.fromString("CRSW")],
       ["keyCode", [Shape2.fromString("SWCR")], Shape2.fromString("CRSW")],
+      ["isOmni", [Shape2.fromString("CRSW")], true],
+      ["isOmni", [Shape2.fromString("CCSW")], false],
     ];
 
     let testNum = 0;
